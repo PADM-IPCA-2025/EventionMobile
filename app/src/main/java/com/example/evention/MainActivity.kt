deede47d20365c8410f2ddf1868d631fcb72c613
pedromartins11 <a23527@alunos.ipca.pt>
2025-04-28 10:39:22 +0100

    Main activity updated

--- app/src/main/java/com/example/evention/MainActivity.kt
+++ app/src/main/java/com/example/evention/MainActivity.kt
@@ -21,104 +21,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EventionTheme {
-                val addressEvent = listOf(
-                    AddressEvent(
-                        "123",
-                        "Rua Dr. Augusto Monteiro",
-                        123,
-                        "4750",
-                        "Barcelos"
-                    ),
-                    AddressEvent(
-                        "456",
-                        "Rua Do Queimado",
-                        123,
-                        "4750",
-                        "Barcelos"
-                    ),
-                    AddressEvent(
-                        "789",
-                        "Rua Barbosa Almeida",
-                        123,
-                        "4750",
-                        "Barcelos"
-                    ),
-                )
 
-                val eventStatus = listOf(
-                    EventStatus(
-                        "123",
-                        "Pendente"
-                    ),
-                    EventStatus(
-                        "456",
-                        "Aprovado"
-                    ),
-                    EventStatus(
-                        "789",
-                        "Completo"
-                    ),
-                )
-
-                val sampleEvents = listOf(
-                    Event(
-                        eventID = "1",
-                        userId = "u1",
-                        name = "Sunset Festival",
-                        description = "A great outdoor event.",
-                        startAt = Date(),
-                        endAt = Date(),
-                        price = 15.0,
-                        createdAt = Date(),
-                        eventStatus = eventStatus[1],
-                        eventstatus_id = "1",
-                        addressEvents = listOf(addressEvent[0], addressEvent[1]),
-                        eventPicture = null
-                    ),
-                    Event(
-                        eventID = "2",
-                        userId = "u2",
-                        name = "Tech Meetup",
-                        description = "Networking for developers.",
-                        startAt = Date(),
-                        endAt = Date(),
-                        price = 0.0,
-                        createdAt = Date(),
-                        eventStatus = eventStatus[1],
-                        eventstatus_id = "2",
-                        addressEvents = listOf(addressEvent[1], addressEvent[2]),
-                        eventPicture = null
-                    ),
-                    Event(
-                        eventID = "2",
-                        userId = "u2",
-                        name = "Tech Meetup",
-                        description = "Networking for developers.",
-                        startAt = Date(),
-                        endAt = Date(),
-                        price = 0.0,
-                        createdAt = Date(),
-                        eventStatus = eventStatus[1],
-                        eventstatus_id = "2",
-                        addressEvents = listOf(addressEvent[1], addressEvent[2]),
-                        eventPicture = null
-                    ),
-                    Event(
-                        eventID = "2",
-                        userId = "u2",
-                        name = "Tech Meetup",
-                        description = "Networking for developers.",
-                        startAt = Date(),
-                        endAt = Date(),
-                        price = 0.0,
-                        createdAt = Date(),
-                        eventStatus = eventStatus[1],
-                        eventstatus_id = "2",
-                        addressEvents = listOf(addressEvent[1], addressEvent[2]),
-                        eventPicture = null
-                    )
-                )
-                HomeScreen(sampleEvents)
             }
         }
     }
@@ -128,9 +31,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Preview() {
     EventionTheme {
-        PaymentScreen(listOf(
-            PaymentMethod("Paypal", true),
-            PaymentMethod("Credit Card", false)
-        ))
+
     }
 }
