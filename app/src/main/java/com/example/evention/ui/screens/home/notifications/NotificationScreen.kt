0f48e431de4d7b55bc53e6082094eb0f8169834f
pedromartins11 <a23527@alunos.ipca.pt>
2025-05-06 11:34:19 +0100

    No notifications screen

--- app/src/main/java/com/example/evention/ui/screens/home/notifications/NotificationScreen.kt
+++ app/src/main/java/com/example/evention/ui/screens/home/notifications/NotificationScreen.kt
@@ -1,5 +1,6 @@
 package com.example.evention.ui.screens.home.notifications
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -20,9 +22,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.evention.R
 import com.example.evention.ui.components.notifications.NotificationRow
 import com.example.evention.ui.theme.EventionTheme
 
@@ -64,10 +68,34 @@ fun NotificationScreen(notifications: List<NotificationItem>, modifier: Modifier
             Spacer(modifier = Modifier.width(28.dp))
         }
 
-        LazyColumn {
-            items(notifications.size) { index ->
-                NotificationRow(notification = notifications[index])
-                HorizontalDivider(modifier = Modifier.padding(vertical = 8.dp))
+        if (notifications.isEmpty()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.nonotification),
+                        contentDescription = "No notifications"
+                    )
+                    Spacer(modifier = Modifier.height(5.dp))
+                    Text(
+                        text = "No notifications",
+                        style = MaterialTheme.typography.titleLarge,
+                        fontWeight = FontWeight.Bold
+                    )
+                }
+            }
+        } else {
+            LazyColumn {
+                items(notifications.size) { index ->
+                    NotificationRow(notification = notifications[index])
+                    HorizontalDivider(modifier = Modifier.padding(vertical = 8.dp))
+                }
             }
         }
     }
