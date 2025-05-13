e6c1a5b79bdcc71243493171aedb38f1e2d7b6c9
Diogo Gomes <a23893@alunos.ipca.pt>
2025-05-13 20:47:28 +0100

    user events screen updated

---
+++ app/src/main/java/com/example/evention/ui/screens/profile/user/UserEvents.kt
@@ -0,0 +1,79 @@
+package com.example.evention.ui.screens.profile.user
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.evention.R
+import com.example.evention.mock.MockData
+import com.example.evention.model.Event
+import com.example.evention.ui.components.TitleComponent
+import com.example.evention.ui.components.admin.events.EventListRow
+import com.example.evention.ui.theme.EventionTheme
+
+@Composable
+fun UserEvents(events: List<Event>) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 25.dp, vertical = 18.dp)
+    ) {
+
+        TitleComponent("Events", true)
+
+        if (events.isEmpty()) {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.noevents),
+                        contentDescription = "No Events"
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = "No events yet",
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                }
+            }
+
+        } else {
+            LazyColumn {
+                items(events.size) { index ->
+                    EventListRow(
+                        event = events[index],
+                        firstSection = "Edit event",
+                        secondSection = "Delete event",
+                        onEdit = { /* ação para editar */ },
+                        onRemove = { /* ação para remover */ }
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun UserEventsPreview() {
+    EventionTheme {
+        var events = MockData.events
+        UserEvents(events)
+    }
+}
