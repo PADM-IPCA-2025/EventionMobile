3a39719b06d5ee945f05100e57a366f69ed52860
pedromartins11 <a23527@alunos.ipca.pt>
2025-06-07 13:19:34 +0100

    test

--- app/src/main/java/com/example/evention/ui/navigation/AppNavHost.kt
+++ app/src/main/java/com/example/evention/ui/navigation/AppNavHost.kt
@@ -54,7 +54,7 @@ fun AppNavHost() {
     val navController = rememberNavController()
     val context = LocalContext.current
 
-    NavHost(navController = navController, startDestination = "home") {
+    NavHost(navController = navController, startDestination = "signIn") {
         composable("splash") {
             SplashScreen(navController = navController)
         }
