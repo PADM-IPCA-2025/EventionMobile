7ede06779f20cd0ff89fbebed3a136ee50464bf9
Diogo Gomes <a23893@alunos.ipca.pt>
2025-06-19 12:12:54 +0100

    small update

--- app/src/main/java/com/example/evention/MainActivity.kt
+++ app/src/main/java/com/example/evention/MainActivity.kt
@@ -4,7 +4,6 @@ import UserPreferences
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import com.example.evention.di.NetworkModule
 import com.example.evention.ui.navigation.AppNavHost
 import com.example.evention.ui.theme.EventionTheme
